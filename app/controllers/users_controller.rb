c14ff0c8965c33c123761cc43252fefd0d369e86
PreethamKarnekanti <preetham.karnekanti@zemosolabs.com>
2021-09-21 20:51:49 +0530

    final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def new
         @user=User.new
     end
     def edit
-        @user=User.find(params[:id])
     end
 
     def index
@@ -11,12 +13,10 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles
     end
 
     def update
-        @user=User.find(params[:id])
         if @user.update(user_params)
             flash[:notice]="Succescfully Updated"
             redirect_to user_path(@user)
@@ -28,16 +28,33 @@ class UsersController < ApplicationController
     def create
         @user=User.new(user_params)
         if @user.save
+            session[:user_id]=@user.id
             flash[:notice]="Signup successfull"
             redirect_to articles_path
         else
             render "new"
         end
     end
+
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice]="Account and associated articles are deleted"
+        redirect_to articles_path
+    end
     private
     def user_params
         params.require(:user).permit(:username ,:email, :password)
 
     end
-
+    def set_user
+        @user=User.find(params[:id])
+    end
+    
+    def require_same_user
+        if current_user!=@user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your account"
+            redirect_to user_path(@user)
+        end
+    end
 end
